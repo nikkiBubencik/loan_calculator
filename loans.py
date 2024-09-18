3c7ea79a626cd1e208a3d05870fca5be1f40bbfc
nikkiBubencik <114317457+nikkiBubencik@users.noreply.github.com>
2024-09-18 08:53:47 -0400

    Update loans.py
    add methods and some attributes and update make_payment

--- loans.py
+++ loans.py
@@ -1,51 +1,44 @@
-principal = 3500
-interest = 0
-rate = .0275
-payment = 121
-
-weeks_to_pay = 0
-while principal + interest > 0:
-    sub_payment = payment
-    if interest > payment:
-        interest -= payment
-    else:
-        sub_payment -= interest
-        interest = 0
-        
-        principal -= sub_payment
-    
-    interest += (principal * rate / 365) *7
-    weeks_to_pay+=1
-    print(weeks_to_pay, principal, interest)
-
-print(weeks_to_pay)
-
 class Loan():
-    def __init__(self, principal_float, interest_float, interest_rate, start_date, minimum_float) -> flaot:
+    def __init__(self, name_str principal_float, interest_float, interest_rate, start_date, minimum_float) -> None:
+        self.name = name_str
         self.principal_float = principal_float
         self.interest_float = interest_float
         self.interest_rate = interest_rate
         self.start_date = start_date
         self.minimum_float = minimum_float
+        self.paid_off_status = False
         
     def make_payment(self, payment_float):
-        sub_payment = payment
-        if interest > payment:
-            self.interest -= payment
+        if self.interest_float > payment_float:
+            self.interest_float -= payment_float
+            payment_float = 0
         else:
-            sub_payment -= interest
-            self.interest = 0
+            payment_float -= interest
+            self.interest_float = 0
 
-            if sub_payment > self.principal:
-                sub_payment -= self.principal
-                self.pricipal = 0
+            if payment_float > self.principal_float:
+                payment_float -= self.principal_float
+                self.principal_float = 0
+                self.paid_off_status = True
             else:
-                self.principal -= sub_payment
-                sub_payment = 0
-        return selfpayment
+                self.principal_float -= payment_float
+                payment_float = 0
+        return (self.status, payment_float)
     
-    def add_interest(self):
-        pass
-    
+    def add_interest(self, frequency):
+        if (frequency == 'weekly'):
+            self.interest_float += self.principal_float * self.interest_rate / 365 * 7
+        elif (frequency == 'biweekly'):
+            self.interest_float += self.principal_float * self.interest_rate / 365 * 14
+        else:
+            self.interest_float += self.principal_float * self.interest_rate / 12
+        
+    def get_total_left(self):
+        return self.principal_float + self.interest_float
 
-    
+    def get_interest(self):
+        return self.interest_float
+
+    def get_name(self):
+        return self.name
+        
