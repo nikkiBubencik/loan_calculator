2b41606005d20b443d7dee397f31ada67eb6794c
nikkiBubencik <114317457+nikkiBubencik@users.noreply.github.com>
2024-09-25 11:12:14 -0400

    Update loan_calculator.py
    fixed errors and added test print

--- loan_calculator.py
+++ loan_calculator.py
@@ -1,16 +1,17 @@
 """include docstrings"""
-import Loan from loans
+from loans import Loan
 
 def get_loans():
   """Fucntion to get the loan information for each loan from user
   returns Loans as a dictionary key: loan name and value: loan type class"""
-  num_loans = int(input("How many loans do you have"))
+  num_loans = input("How many loans do you have ")
+  num_loans = int(num_loans)
   loan_dict = {}
-  
+
   for i in range(num_loans):
     loan_name = input("Enter the name of your loan: ")
-    loan_amount = float(input("Enter the principal loan amount \(ex. 15500\): $"))
-    interest_rate = float(input("Enter your loan interest rate as percentage\(ex. 4.4\) "))
+    loan_amount = float(input("Enter the principal loan amount (ex. 15500): $"))
+    interest_rate = float(input("Enter your loan interest rate as percentage(ex. 4.4) "))
     interest_amount = float(input("Enter amount of interest already accured $"))
     start_date = input("enter date the loan starts repayment(if started already enter today's date) ")
     minimum_due = float(input("Enter minimum payment when loan is in repayment "))
@@ -19,22 +20,11 @@ def get_loans():
     loan_dict[loan_name] = Loan(loan_name, loan_amount, interest_amount, interest_rate, start_date, minimum_due)
   return loan_dict
   
-loan_dict = get_loans()
-# active_loans = loan_dict.values()
-
-payment_option = ['weekly', 'biweekly', 'monthly']
-print("Payment frequency options:")
-for j in range(len(payment_options)):
-  print(f"{j + 1}. {payment_options[j]")
-
-payment_freq_num = int(input(f"Pick a payment frequecy between 1 and {len(payment_options)} "))
-payment_freq = payment_options[payment_freq_num - 1]
-payment_amount = float(input("How much will you contribute each time: $"))
 def sort_loans_by_interest_rate(loan_dict):
     """Sorts the loans from high to low interest rate."""
     
     # Sort the dictionary by interest rate and return a list of loan_object
-    sorted_loans = sorted(loan_dict.values(), key=lambda item: (item[1].interest_rate, item[i].principal_float), reverse=True)
+    sorted_loans = sorted(loan_dict.values(), key=lambda item: (item.interest_rate, item.principal_float), reverse=True)
     
     return sorted_loans
   
@@ -46,15 +36,29 @@ def avalanche(loan_dict):
   current_date = 0
   
   while len(active_loans_list) > 0:
-    sub_payment = payment_amount
-    for i inrange(len(active_loans_list)):
+    sub_payment:float = payment_amount
+    for i in range(len(active_loans_list)):
       sub_payment = active_loans_list[i].make_payment(sub_payment)
       if active_loans_list[i].get_total_left() == 0:
-        paid_off_loans = {active_loans_list[i], current_date)
+        paid_off_loans_list.append((active_loans_list[i], current_date))
         del active_loans_list[i]
         
-    curretn_date += 1
+    current_date += 1
   return paid_off_loans_list
+
+
+loan_dict = get_loans()
+# active_loans = loan_dict.values()
+
+payment_options = ['weekly', 'biweekly', 'monthly']
+print("Payment frequency options:")
+for j in range(len(payment_options)):
+  print(f"{j + 1}. {payment_options[j]}")
+
+payment_freq_num = int(input(f"Pick a payment frequecy between 1 and {len(payment_options)} "))
+payment_freq = payment_options[payment_freq_num - 1]
+payment_amount = float(input("How much will you contribute each time: $"))
+
 paid_off_loans = avalanche(loan_dict)
-for loan, paid_off_date in paid_off_loans:
-  print(f"Loan {loan.name} paid off in {paid_off_date}")
+print(paid_off_loans)
+  
